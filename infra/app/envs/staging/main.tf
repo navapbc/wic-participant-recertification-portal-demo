38a7edea86860d30e2b6793405faf8c467a2886b
Alsia Plybeah <alsia@navapbc.com>
2023-01-11 10:35:09 -0500

    PRP-13 Add Database (#8)
    Ticket
    https://wicmtdp.atlassian.net/browse/PRP-13
    
    Changes
    Add Aurora Serverless DB
    add staging module

--- infra/app/envs/staging/main.tf
+++ infra/app/envs/staging/main.tf
@@ -2,6 +2,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
+  environment_name = "staging"
   # The prefix key/value pair is used for terraform workspaces, which is useful for projects with multiple infrastructure developers. 
   # Leave this as a static string if you are not using workspaces for this environment (recommended). Change it to terraform.workspace 
   # if you want to use workspaces in this environment.
@@ -48,3 +49,8 @@ module "project_config" {
 }
 
 # Add application modules below
+module "app" {
+  source           = "../../env-template"
+  environment_name = local.environment_name
+  # image_tag        = local.image_tag # this doesn't exist yet
+}
--- infra/modules/service/main.tf
+++ infra/modules/service/main.tf
@@ -319,3 +319,150 @@ resource "aws_security_group" "app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+###########################
+## Database Configuration ##
+###########################
+resource "aws_rds_cluster" "postgresql" {
+  # checkov:skip=CKV2_AWS_27:have concerns about sensitive data in logs; want better way to get this information
+  # checkov:skip=CKV2_AWS_8:TODO add backup selection plan using tags
+  cluster_identifier = var.service_name
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  database_name      = replace("${var.service_name}", "-", "_")
+  master_username    = "app_usr"
+  master_password    = aws_ssm_parameter.random_db_password.value
+  storage_encrypted  = true
+  # checkov:skip=CKV_AWS_128:IAM Auth will be added with the `wic-prp-eng` role created in PRP-74 https://wicmtdp.atlassian.net/browse/PRP-74
+  # checkov:skip=CKV_AWS_162:IAM Auth will be added with the `wic-prp-eng` role created in PRP-74 https://wicmtdp.atlassian.net/browse/PRP-74
+  # iam_database_authentication_enabled = true
+  deletion_protection = true
+
+
+  serverlessv2_scaling_configuration {
+    max_capacity = 1.0
+    min_capacity = 0.5
+  }
+
+}
+
+resource "aws_rds_cluster_instance" "postgresql-cluster" {
+  cluster_identifier         = aws_rds_cluster.postgresql.id
+  instance_class             = "db.serverless"
+  engine                     = aws_rds_cluster.postgresql.engine
+  engine_version             = aws_rds_cluster.postgresql.engine_version
+  auto_minor_version_upgrade = true
+  monitoring_role_arn        = aws_iam_role.rds_enhanced_monitoring.arn
+  monitoring_interval        = 30
+}
+
+resource "random_password" "random_db_password" {
+  length           = 48
+  special          = true
+  min_special      = 6
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "random_db_password" {
+  name  = "/metadata/db/admin-password"
+  type  = "SecureString"
+  value = random_password.random_db_password.result
+}
+
+################################################################################
+# Backup Configuration
+################################################################################
+
+resource "aws_backup_plan" "postgresql" {
+  name = "${var.service_name}_backup_plan"
+
+  rule {
+    rule_name         = "${var.service_name}_backup_rule"
+    target_vault_name = "${var.service_name}-vault"
+    schedule          = "cron(0 12 ? * SUN *)"
+  }
+}
+# create IAM role
+resource "aws_iam_role" "postgresql_backup" {
+  name_prefix        = "aurora-backup-"
+  assume_role_policy = data.aws_iam_policy_document.postgresql_backup.json
+}
+
+resource "aws_iam_role_policy_attachment" "postgresql_backup" {
+  role       = aws_iam_role.postgresql_backup.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+}
+
+data "aws_iam_policy_document" "postgresql_backup" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["backup.amazonaws.com"]
+    }
+  }
+}
+# backup selection
+resource "aws_backup_selection" "postgresql_backup" {
+  iam_role_arn = aws_iam_role.postgresql_backup.arn
+  name         = "${var.service_name}-backup"
+  plan_id      = aws_backup_plan.postgresql.id
+
+  resources = [
+    aws_rds_cluster.postgresql.arn
+  ]
+}
+
+################################################################################
+# IAM role for enhanced monitoring
+################################################################################
+
+resource "aws_iam_role" "rds_enhanced_monitoring" {
+  name_prefix        = "aurora-enhanced-monitoring-"
+  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
+}
+
+resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
+  role       = aws_iam_role.rds_enhanced_monitoring.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
+data "aws_iam_policy_document" "rds_enhanced_monitoring" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["monitoring.rds.amazonaws.com"]
+    }
+  }
+}
+
+################################################################################
+# Parameters for Query Logging
+################################################################################
+
+resource "aws_rds_cluster_parameter_group" "rds_query_logging" {
+  name        = var.service_name
+  family      = "aurora-postgresql13"
+  description = "Default cluster parameter group"
+
+  parameter {
+    name  = "log_statement"
+    value = "all"
+  }
+
+  parameter {
+    name  = "log_min_duration_statement"
+    value = "1"
+  }
+}
